cfaac495d14c09ee59153bb3b6d3520bdb5601f2
Omar630603 <omar.yem1111@gmail.com>
2021-05-08 22:34:17 +0700

    service migration

---
+++ database/migrations/2021_05_08_152638_create_service_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateServiceTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('service', function (Blueprint $table) {
+            $table->id('service_id');
+            $table->unsignedBigInteger('category_service_id');
+            $table->foreign('category_service_id')->references('category_service_id')->on('category_service')->onDelete('cascade')->onUpdate('cascade');
+            $table->string('name');
+            $table->integer('price');
+            $table->string('image')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('service');
+    }
+}
