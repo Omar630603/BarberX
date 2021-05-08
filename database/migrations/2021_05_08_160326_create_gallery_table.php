f97b949798f7dd25e748eee11fae507f35bf2e64
Omar630603 <omar.yem1111@gmail.com>
2021-05-08 23:04:56 +0700

    gallery migration

---
+++ database/migrations/2021_05_08_160326_create_gallery_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGalleryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('gallery', function (Blueprint $table) {
+            $table->id('gallery_id');
+            $table->unsignedBigInteger('category_service_id');
+            $table->foreign('category_service_id')->references('category_service_id')->on('category_service')->onDelete('cascade')->onUpdate('cascade');
+            $table->string('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('gallery');
+    }
+}
